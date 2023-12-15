9eb1ca5e6155acdd69e9ab53ff0d481ff78d3256
Himanshu Pandey <himanshu21pandey@gmail.com>
2023-12-15 19:25:07 +0530

    Selection working with delay

--- src/components/ScannerResult.tsx
+++ src/components/ScannerResult.tsx
@@ -1,4 +1,5 @@
 import { Button, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/react";
+import { useState } from "react";
 import { Toaster, toast } from "sonner";
 import useSWR from "swr";
 
@@ -6,19 +7,39 @@ const fetcher = (...args: Parameters<typeof fetch>) =>
     fetch(...args).then((res) => res.json());
 export default function ScannerResult({ url }: { url: string }) {
     const { data, error, isLoading }: { data: StockDetail[], error: string | undefined, isLoading: boolean } = useSWR(url, fetcher)
+    const [selectedStocks, setSelectedStocks] = useState<Set<never> | 'all'>(new Set([]));
+    const [selectedSymbolToCopy, setSelectedSymbolToCopy] = useState<StockDetail[]>([])
     if (error) return "Error..."
     else if (isLoading) return "Loading..."
-    const symbols = data.map(item => item.Symbol)
-    const copyData = symbols.join(', ')
     const copyToClipboard = () => {
+        const symbols = selectedSymbolToCopy.map(item => item.Symbol)
+        const copyData = symbols.join(', ')
         navigator.clipboard.writeText(copyData)
         toast.success('List copied to clipboard', { duration: 1000 })
+    }
+    console.log(selectedSymbolToCopy.length)
+    const handleSelectionChange = (keys: Set<never> | "all") => {
+        setSelectedStocks(keys)
+        if (selectedStocks === 'all') setSelectedSymbolToCopy(data)
+        else {
+            const tmpStocks: StockDetail[] = []
+            selectedStocks.forEach((x) => {
+                tmpStocks.push(data[x])
+            })
+            setSelectedSymbolToCopy(tmpStocks)
+        }
     }
     return (
         <div className="grid">
             <Toaster position="bottom-right" />
             <Button className="justify-self-end mb-4" radius="sm" size="sm" color="primary" onClick={copyToClipboard}>Copy</Button>
-            <Table aria-label="Example static collection table">
+            <Table
+                aria-label="Example static collection table"
+                selectedKeys={selectedStocks}
+                selectionMode="multiple"
+                selectionBehavior="toggle"
+                onSelectionChange={(keys) => handleSelectionChange(keys as Set<never> | "all")}
+            >
                 <TableHeader>
                     <TableColumn>Name</TableColumn>
                     <TableColumn>Symbol</TableColumn>
